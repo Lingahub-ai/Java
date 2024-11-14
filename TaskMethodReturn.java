beba73185e5abbb967c5b0bcc74a5dda1cadf6de
Lingahub-ai <lingaa4444@gmail.com>
2024-11-15 00:30:17 +0530

    Add files via upload

---
+++ TaskMethodReturn.java
@@ -0,0 +1,16 @@
+//14/11/24-Task-write a program to add 2 number using return type-
+package AllProgram;
+
+public class TaskMethodReturn {
+	
+		static int addnum(int a,int b) {
+
+		return a+b;
+
+		}public static void main(String [] args)
+			{
+				int a=5,b=10;
+			int res=addnum(a,b);
+		System.out.println("Answer"+res);
+	}
+	}
